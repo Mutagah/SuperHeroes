7da1abc156f2abc290878059136309fbc97e5faa
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-10-08 16:54:44 +0300

    Milestone:Edited the create method in hero_powers to check if the associated foreign_id are present

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,9 +1,16 @@
 class HeroPowersController < ApplicationController
-rescue_from ActiveRecord::RecordInvalid,  with: :invalid_record
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+rescue_from ActiveRecord::RecordNotFound, with: :invalid_record
     def create
-        heropower = HeroPower.create!(heropower_params)
+        heropower = HeroPower.new(heropower_params)
         hero = Hero.find(heropower.hero_id)
-        render json: hero,serializer: HeroheroSerializer,status: :created
+        power = Power.find(heropower.power_id)
+        if hero && power
+            heropower.save
+            render json: hero,serializer: HeroheroSerializer,status: :created
+        else
+            invalid_record
+        end
     end
 
     private
@@ -15,4 +22,8 @@ rescue_from ActiveRecord::RecordInvalid,  with: :invalid_record
     def invalid_record
         render json: {errors: ["validation errors"]}, status: :unprocessable_entity
     end
+
+    # def unfound_response
+    #     render json: {errors: "The hero_id and the power_id does not exist"}
+    # end
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -2,6 +2,7 @@ class Power < ApplicationRecord
     has_many :hero_powers
     has_many :heros, through: :hero_powers
     validates :description, presence: true ,length: {minimum: 20}
+    # validates :name, absence: true
 end
 
 # gives the wielder super-human strengths
